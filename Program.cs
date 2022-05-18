8dbc2841dab50037de699c05ba7c0fb3f7b8b741
Shubhamrazz <sr99845@gmail.com>
2022-05-18 22:16:11 +0530

    [Shubham]Enter valid First Name

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegexUserRegistrationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Welcome To User Registration Program----");
+            UserRegistration.ValidateFirstName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistrationProgram
+{
+    class UserRegistration
+    {
+        public static void ValidateFirstName()
+        {
+            string Pattern = "^[a-zA-Z]{3,}?";//[^!@#$%^&*0-9]
+            string[] inputs = { "Om", "Shubham", "Adyasha", "Priyanka", "Akansha", "Raj", "Shalini", "Mehek", "6nupur", "@Gagan", "Sim" };
+            foreach (string input in inputs)
+            {
+                bool Result = Regex.IsMatch(input, Pattern);
+                if (Result)
+                {
+                   Console.WriteLine("{0} is valid", input);
+                }
+                else
+                  Console.WriteLine("{0} is invalid", input);
+            }
+        }
+        
+    }
+}
